d3d75fe3fdb968e4d0df26d34fd3962ce2e973fd
David Farrell <davidnmfarrell@gmail.com>
2019-05-28 14:32:36 -0400

    Optimizations
    - the file is in latin1 encoding
    - match a regex for non-exact matches
    - rename variables for clarity

--- webster-search.pl
+++ webster-search.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-my $search_word = shift or return;
-$search_word = uc "$search_word\n";
+my $search_term = uc join(' ',@ARGV) . "\n";
+my $entry_pattern = qr/^[A-Z][A-Z0-9' ;-]*$/;
+my $search_pattern = qr/^$search_term/;
 
 my %index = (
   A => 601,
@@ -36,28 +37,26 @@ my %index = (
 my @script_path = split '/', $0;
 pop @script_path;
 my $dictionary_path = join '/',  @script_path, 'webster-1913.txt';
-
-my $start = $index{ substr $search_word, 0, 1 };
-open my $dict, '<', $dictionary_path or die $!;
+open my $dict, '<:encoding(latin1)', $dictionary_path or die $!;
+my $start = $index{ substr $search_term, 0, 1 };
 seek $dict, $start, 0;
 
-my $found_entry = undef;
-my $entry_pattern = qr/^[A-Z][A-Z0-9 ;-]*\n/;
+my $found_match = undef;
 while (<$dict>) {
-  next unless /$entry_pattern/;
+  next unless $_ =~ $entry_pattern;
 
-  if ($search_word eq $_) {
-    $found_entry = 1;
-    my $text = $_;
+  if ($_ =~ $search_term) {
+    my $output = $_;
     while (1) {
      my $next_line = readline $dict;
      if ($next_line =~ /$entry_pattern/) {
        seek $dict, -length($next_line), 1;
        last;
      }
-     $text .= $next_line;
+     $output .= $next_line;
     }
-    print $text;
+    print $output;
+    $found_match = 1;
   }
-  last if $found_entry && ($search_word cmp $_) == -1;
+  last if $found_match && ($search_term cmp $_) == -1;
 }
